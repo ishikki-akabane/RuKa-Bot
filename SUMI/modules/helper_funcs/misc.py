524ea8f5a46e388ac3ef9884f0eb593711ff8b62
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2022-09-02 08:39:55 +0530

    SUMI @ISHIKKI_AKABANE

--- SUMI/modules/helper_funcs/misc.py
+++ SUMI/modules/helper_funcs/misc.py
@@ -71,7 +71,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if calc in [1, 2]:
         pairs.append((modules[-1],))
 
-    max_num_pages = ceil(len(pairs) / 6)
+    max_num_pages = ceil(len(pairs) / 8)
     modulo_page = page_n % max_num_pages
 
     # can only have a certain amount of buttons side by side
