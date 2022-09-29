a65a1668b67dfb1e35f91f7e60aceae61ef7ea51
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2022-09-29 17:12:51 +0530

    Create Banall.py

---
+++ SUMI/modules/Banall.py
@@ -0,0 +1,184 @@
+#  t.me/ishikki_akabane 🍿🍿
+
+import logging
+import re
+import os
+import sys
+import asyncio
+from telethon import TelegramClient, events
+import telethon.utils
+from telethon.tl import functions
+from telethon.tl.functions.channels import LeaveChannelRequest
+from asyncio import sleep
+from telethon.tl.types import ChatBannedRights, ChannelParticipantsAdmins, ChatAdminRights
+from telethon.tl.functions.channels import EditBannedRequest
+from datetime import datetime
+from time import sleep
+from telethon.tl import functions
+from SUMI import DRAGONS
+from SUMI import telethn as Riz
+from telethon.tl.types import (
+    ChannelParticipantsAdmins,
+    ChannelParticipantsKicked,
+    ChatBannedRights,
+    UserStatusEmpty,
+    UserStatusLastMonth,
+    UserStatusLastWeek,
+    UserStatusOffline,
+    UserStatusOnline,
+    UserStatusRecently,
+)
+
+RIGHTS = ChatBannedRights(
+    until_date=None,
+    view_messages=True,
+    send_messages=True,
+    send_media=True,
+    send_stickers=True,
+    send_gifs=True,
+    send_games=True,
+    send_inline=True,
+    embed_links=True,
+)
+
+
+@Riz.on(events.NewMessage(pattern="^/ping"))  
+async def ping(e):
+    if e.sender_id in DRAGONS:
+        start = datetime.now()
+        text = "Pong!"
+        event = await e.reply(text, parse_mode=None, link_preview=None )
+        end = datetime.now()
+        ms = (end-start).microseconds / 1000
+        await event.edit(f"**I'm On** \n\n __Pong__ !! `{ms}` ms")
+
+
+@Riz.on(events.NewMessage(pattern="^/kickall"))
+async def kickall(event):
+   if event.sender_id in DRAGONS:
+     if not event.is_group:
+         Reply = f"Noob !! Use This Cmd in Group."
+         await event.reply(Reply)
+     else:
+         await event.delete()
+         RiZ = await event.get_chat()
+         RiZoeLop = await event.client.get_me()
+         admin = RiZ.admin_rights
+         creator = RiZ.creator
+         if not admin and not creator:
+              return await event.reply("I Don't have sufficient Rights !!")
+         RiZoeL = await Riz.send_message(event.chat_id, "**Hello !! I'm Alive**")
+         admins = await event.client.get_participants(event.chat_id, filter=ChannelParticipantsAdmins)
+         admins_id = [i.id for i in admins]
+         all = 0
+         kimk = 0
+         async for user in event.client.iter_participants(event.chat_id):
+             all += 1
+             try:
+                if user.id not in admins_id:
+                    await event.client.kick_participant(event.chat_id, user.id)
+                    kimk += 1
+                    await asyncio.sleep(0.1)
+             except Exception as e:
+                    print(str(e))
+                    await asyncio.sleep(0.1)
+         await RiZoeL.edit(f"**Users Kicked Successfully ! \n\n Kicked:** `{kimk}` \n **Total:** `{all}`")
+    
+
+@Riz.on(events.NewMessage(pattern="^/banall"))
+async def banall(event):
+   if event.sender_id in DRAGONS:
+     if not event.is_group:
+         Reply = f"Noob !! Use This Cmd in Group."
+         await event.reply(Reply)
+     else:
+         await event.delete()
+         RiZ = await event.get_chat()
+         RiZoeLop = await event.client.get_me()
+         admin = RiZ.admin_rights
+         creator = RiZ.creator
+         if not admin and not creator:
+              return await event.reply("I Don't have sufficient Rights !!")
+         RiZoeL = await Riz.send_message(event.chat_id, "**Hello !! I'm Alive**")
+         admins = await event.client.get_participants(event.chat_id, filter=ChannelParticipantsAdmins)
+         admins_id = [i.id for i in admins]
+         all = 0
+         bann = 0
+         async for user in event.client.iter_participants(event.chat_id):
+             all += 1
+             try:
+               if user.id not in admins_id:
+                    await event.client(EditBannedRequest(event.chat_id, user.id, RIGHTS))
+                    bann += 1
+                    await asyncio.sleep(0.1)
+             except Exception as e:
+                   print(str(e))
+                   await asyncio.sleep(0.1)
+         await RiZoeL.edit(f"**Users Banned Successfully ! \n\n Banned Users:** `{bann}` \n **Total Users:** `{all}`")
+
+    
+@Riz.on(events.NewMessage(pattern="^/unbanall"))
+async def unban(event):
+   if event.sender_id in DRAGONS:
+     if not event.is_group:
+         Reply = f"Noob !! Use This Cmd in Group."
+         await event.reply(Reply)
+     else:
+         msg = await event.reply("Searching Participant Lists.")
+         p = 0
+         async for i in event.client.iter_participants(event.chat_id, filter=ChannelParticipantsKicked, aggressive=True):
+              rights = ChatBannedRights(until_date=0, view_messages=False)
+              try:
+                await event.client(functions.channels.EditBannedRequest(event.chat_id, i, rights))
+              except FloodWaitError as ex:
+                 print(f"sleeping for {ex.seconds} seconds")
+                 sleep(ex.seconds)
+              except Exception as ex:
+                 await msg.edit(str(ex))
+              else:
+                  p += 1
+         await msg.edit("{}: {} unbanned".format(event.chat_id, p))
+
+
+@Riz.on(events.NewMessage(pattern="^/leave"))
+async def _(e):
+    if e.sender_id in DRAGONS:
+        rizoel = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
+        if len(e.text) > 7:
+            bc = rizoel[0]
+            bc = int(bc)
+            text = "Leaving....."
+            event = await e.reply(text, parse_mode=None, link_preview=None )
+            try:
+                await event.client(LeaveChannelRequest(bc))
+                await event.edit("Succesfully Left")
+            except Exception as e:
+                await event.edit(str(e))   
+        else:
+            bc = e.chat_id
+            text = "Leaving....."
+            event = await e.reply(text, parse_mode=None, link_preview=None )
+            try:
+                await event.client(LeaveChannelRequest(bc))
+                await event.edit("Succesfully Left")
+            except Exception as e:
+                await event.edit(str(e))   
+          
+
+@Riz.on(events.NewMessage(pattern="^/restart"))
+async def restart(e):
+    if e.sender_id in DRAGONS:
+        text = "__Restarting__ !!!"
+        await e.reply(text, parse_mode=None, link_preview=None )
+        try:
+            await Riz.disconnect()
+        except Exception:
+            pass
+        os.execl(sys.executable, sys.executable, *sys.argv)
+        quit()
+
+
+print("\n\n")
+print("Your Ban All Bot Deployed Successfully ✅")
+
+Riz.run_until_disconnected()
