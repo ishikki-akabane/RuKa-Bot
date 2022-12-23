b8582f7c33966de1eb43bc6ffbc99a1370932263
Ishikki Akabane <rohansjaishwal@gmail.com>
2022-12-23 17:21:41 +0530

    Update mongo.py

--- RUKA/mongo.py
+++ RUKA/mongo.py
@@ -3,8 +3,8 @@ import sys
 from motor import motor_asyncio
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from SUMI import MONGO_DB_URI
-from SUMI.conf import get_int_key, get_str_key
+from RUKA import MONGO_DB_URI
+from RUKA.conf import get_int_key, get_str_key
 
 
 MONGO_PORT = get_int_key("27017")
