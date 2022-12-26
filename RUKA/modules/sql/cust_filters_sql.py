70e4ae4f214a354f8c5f517f51ccf4434d70c9cb
Ishikki Akabane <rohansjaishwal@gmail.com>
2022-12-26 19:21:09 +0530

    Update cust_filters_sql.py

--- RUKA/modules/sql/cust_filters_sql.py
+++ RUKA/modules/sql/cust_filters_sql.py
@@ -2,8 +2,8 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Boolean, Integer, distinct, func
 
-from SUMI.modules.helper_funcs.msg_types import Types
-from SUMI.modules.sql import BASE, SESSION
+from RUKA.modules.helper_funcs.msg_types import Types
+from RUKA.modules.sql import BASE, SESSION
 
 
 class CustomFilters(BASE):
