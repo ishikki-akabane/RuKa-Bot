d72fd6a8c1191c39b7c6a463a613fb05efa66b75
Ishikki Akabane <rohansjaishwal@gmail.com>
2022-12-26 18:57:51 +0530

    Update debug.py

--- RUKA/modules/debug.py
+++ RUKA/modules/debug.py
@@ -5,8 +5,8 @@ from telethon import events
 from telegram import Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
-from SUMI import telethn, dispatcher
-from SUMI.modules.helper_funcs.chat_status import dev_plus
+from RUKA import telethn, dispatcher
+from RUKA.modules.helper_funcs.chat_status import dev_plus
 
 DEBUG_MODE = False
 
