93ea8df74569e87c21d77c7e8a2a4d5c984eee10
Ishikki Akabane <rohansjaishwal@gmail.com>
2022-12-26 19:02:25 +0530

    Update gifkang.py

--- RUKA/modules/gifkang.py
+++ RUKA/modules/gifkang.py
@@ -4,7 +4,7 @@ from typing import Dict, List, Union
 from html import escape
 import ffmpeg
 
-from SUMI import NO_LOAD
+from RUKA import NO_LOAD
 from telegram import MAX_MESSAGE_LENGTH, Bot, InlineKeyboardButton, ParseMode
 from telegram.error import TelegramError
 
