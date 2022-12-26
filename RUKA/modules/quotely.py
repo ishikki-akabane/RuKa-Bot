0a1f089cfb81db2e79d66c73746b6ad7fbfba671
Ishikki Akabane <rohansjaishwal@gmail.com>
2022-12-26 19:08:53 +0530

    Update quotely.py

--- RUKA/modules/quotely.py
+++ RUKA/modules/quotely.py
@@ -8,9 +8,9 @@ import time
 from datetime import datetime as dt
 from random import choice
 from shutil import rmtree
-from SUMI import quotly
-from SUMI.services.quotehelper import eor
-from SUMI.events import register  
+from RUKA import quotly
+from RUKA.services.quotehelper import eor
+from RUKA.events import register  
 
 @register(pattern="^/q(?: |$)(.*)")
 async def quott_(event):
