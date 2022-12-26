bc0f04365798c7d28f7f7c2aade26ecb5c53f4b3
Ishikki Akabane <rohansjaishwal@gmail.com>
2022-12-26 19:31:51 +0530

    Update warns_sql.py

--- RUKA/modules/sql/warns_sql.py
+++ RUKA/modules/sql/warns_sql.py
@@ -3,7 +3,7 @@ import threading
 from sqlalchemy import Integer, Column, String, UnicodeText, func, distinct, Boolean
 from sqlalchemy.dialects import postgresql
 from sqlalchemy.sql.sqltypes import BigInteger
-from SUMI.modules.sql import SESSION, BASE
+from RUKA.modules.sql import SESSION, BASE
 
 
 class Warns(BASE):
