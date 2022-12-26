888fb5af471498d6a9fd0c6076230ff2680c6ab9
Ishikki Akabane <rohansjaishwal@gmail.com>
2022-12-26 19:20:00 +0530

    Update blacklist_sql.py

--- RUKA/modules/sql/blacklist_sql.py
+++ RUKA/modules/sql/blacklist_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import func, distinct, Column, String, UnicodeText, Integer
 
-from SUMI.modules.sql import SESSION, BASE
+from RUKA.modules.sql import SESSION, BASE
 
 
 class BlackListFilters(BASE):
