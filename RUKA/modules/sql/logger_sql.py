965b9be03872ff1fb3750533509d940723bfda31
Ishikki Akabane <rohansjaishwal@gmail.com>
2022-12-26 19:22:16 +0530

    Update logger_sql.py

--- RUKA/modules/sql/logger_sql.py
+++ RUKA/modules/sql/logger_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from SUMI.modules.sql import BASE, SESSION
+from RUKA.modules.sql import BASE, SESSION
 
 class LoggerSettings(BASE):
     __tablename__ = "chat_log_settings"
