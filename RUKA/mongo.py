46fc5ca402129e3127b8ccc5cc7ae7320f8678aa
Ishikki Akabane <rohansjaishwal@gmail.com>
2022-12-26 18:48:05 +0530

    Update mongo.py

--- RUKA/mongo.py
+++ RUKA/mongo.py
@@ -9,14 +9,14 @@ from RUKA.conf import get_int_key, get_str_key
 
 MONGO_PORT = get_int_key("27017")
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "SUMI"
+MONGO_DB = "RUKA"
 
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
-db = client["SUMI"]
+db = client["RUKA"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
