d53ff8b904dd6fcc1229e0fb864512327575f79f
Ishikki Akabane <rohansjaishwal@gmail.com>
2022-12-26 19:02:17 +0530

    Update gettime.py

--- RUKA/modules/gettime.py
+++ RUKA/modules/gettime.py
@@ -2,8 +2,8 @@ import datetime
 from typing import List
 
 import requests
-from SUMI import TIME_API_KEY, dispatcher
-from SUMI.modules.disable import DisableAbleCommandHandler
+from RUKA import TIME_API_KEY, dispatcher
+from RUKA.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
