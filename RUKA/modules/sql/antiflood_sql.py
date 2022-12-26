cd12c477707549287e4013a63641df706af34ef1
Ishikki Akabane <rohansjaishwal@gmail.com>
2022-12-26 19:19:45 +0530

    Update antiflood_sql.py

--- RUKA/modules/sql/antiflood_sql.py
+++ RUKA/modules/sql/antiflood_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import String, Column, Integer, BigInteger, UnicodeText
 
-from SUMI.modules.sql import SESSION, BASE
+from RUKA.modules.sql import SESSION, BASE
 
 DEF_COUNT = 1
 DEF_LIMIT = 0
