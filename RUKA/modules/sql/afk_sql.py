6560d024728a0fb5531de1bb060260c44b56b810
Ishikki Akabane <rohansjaishwal@gmail.com>
2022-12-26 19:19:23 +0530

    Update afk_sql.py

--- RUKA/modules/sql/afk_sql.py
+++ RUKA/modules/sql/afk_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, UnicodeText, Boolean, BigInteger
 
-from SUMI.modules.sql import BASE, SESSION
+from RUKA.modules.sql import BASE, SESSION
 
 
 class AFK(BASE):
