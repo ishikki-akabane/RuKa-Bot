88ec9017f15318a0034b1840a8e9ee63b3e105eb
Ishikki Akabane <rohansjaishwal@gmail.com>
2022-12-26 19:21:02 +0530

    Update connection_sql.py

--- RUKA/modules/sql/connection_sql.py
+++ RUKA/modules/sql/connection_sql.py
@@ -2,7 +2,7 @@ import threading
 import time
 from typing import Union
 from sqlalchemy import Column, String, Boolean, UnicodeText, Integer
-from SUMI.modules.sql import SESSION, BASE
+from RUKA.modules.sql import SESSION, BASE
 from sqlalchemy.sql.sqltypes import BigInteger
 
 
