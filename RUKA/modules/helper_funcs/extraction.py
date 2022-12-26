87d245c2925e35b3d521f7e4596fc90f15b7e042
Ishikki Akabane <rohansjaishwal@gmail.com>
2022-12-26 19:17:13 +0530

    Update extraction.py

--- RUKA/modules/helper_funcs/extraction.py
+++ RUKA/modules/helper_funcs/extraction.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
 
-from SUMI import LOGGER
-from SUMI.modules.users import get_user_id
+from RUKA import LOGGER
+from RUKA.modules.users import get_user_id
 from telegram import Message, MessageEntity
 from telegram.error import BadRequest
 
