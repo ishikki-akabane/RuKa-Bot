0ecf20d6cbff4371870b6a68686361882c0e0f9b
Ishikki Akabane <rohansjaishwal@gmail.com>
2022-12-26 19:35:18 +0530

    Update errors.py

--- RUKA/utils/errors.py
+++ RUKA/utils/errors.py
@@ -2,7 +2,7 @@
 import sys
 import traceback
 from functools import wraps
-from SUMI import pgram, SUPPORT_CHAT
+from RUKA import pgram, SUPPORT_CHAT
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 
 
