16037f56742dbf1c2defd033e2bbe4c654c93663
Ishikki Akabane <rohansjaishwal@gmail.com>
2022-12-26 18:55:00 +0530

    Update blacklistusers.py

--- RUKA/modules/blacklistusers.py
+++ RUKA/modules/blacklistusers.py
@@ -1,12 +1,12 @@
 # Module to blacklist users and prevent them from using commands by @TheRealPhoenix
 import html
-import SUMI.modules.sql.blacklistusers_sql as sql
-from SUMI import (DEV_USERS, OWNER_ID, DRAGONS, DEMONS, TIGERS, WOLVES,
+import RUKA.modules.sql.blacklistusers_sql as sql
+from RUKA import (DEV_USERS, OWNER_ID, DRAGONS, DEMONS, TIGERS, WOLVES,
                           dispatcher)
-from SUMI.modules.helper_funcs.chat_status import dev_plus
-from SUMI.modules.helper_funcs.extraction import (extract_user,
+from RUKA.modules.helper_funcs.chat_status import dev_plus
+from RUKA.modules.helper_funcs.extraction import (extract_user,
                                                           extract_user_and_text)
-from SUMI.modules.log_channel import gloggable
+from RUKA.modules.log_channel import gloggable
 from telegram import ParseMode, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, run_async
