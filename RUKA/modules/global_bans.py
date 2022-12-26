fa4dbf391710320e77a9de2a72219a844652d986
Ishikki Akabane <rohansjaishwal@gmail.com>
2022-12-26 19:03:01 +0530

    Update global_bans.py

--- RUKA/modules/global_bans.py
+++ RUKA/modules/global_bans.py
@@ -14,9 +14,9 @@ from telegram.ext import (
 )
 from telegram.utils.helpers import mention_html
 
-import SUMI.modules.sql.global_bans_sql as sql
-from SUMI.modules.sql.users_sql import get_user_com_chats
-from SUMI import (
+import RUKA.modules.sql.global_bans_sql as sql
+from RUKA.modules.sql.users_sql import get_user_com_chats
+from RUKA import (
     DEV_USERS,
     EVENT_LOGS,
     OWNER_ID,
@@ -30,16 +30,16 @@ from SUMI import (
     sw,
     dispatcher,
 )
-from SUMI.modules.helper_funcs.chat_status import (
+from RUKA.modules.helper_funcs.chat_status import (
     is_user_admin,
     support_plus,
     user_admin,
 )
-from SUMI.modules.helper_funcs.extraction import (
+from RUKA.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from SUMI.modules.helper_funcs.misc import send_to_list
+from RUKA.modules.helper_funcs.misc import send_to_list
 
 GBAN_ENFORCE_GROUP = 6
 
