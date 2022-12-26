5b6044cb2f58543f59ca0332e22c924061b99202
Ishikki Akabane <rohansjaishwal@gmail.com>
2022-12-26 19:14:17 +0530

    Update urban_dictionary.py

--- RUKA/modules/urban_dictionary.py
+++ RUKA/modules/urban_dictionary.py
@@ -1,6 +1,6 @@
 import requests
-from SUMI import dispatcher
-from SUMI.modules.disable import DisableAbleCommandHandler
+from RUKA import dispatcher
+from RUKA.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
