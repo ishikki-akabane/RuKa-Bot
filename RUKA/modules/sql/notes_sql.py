bcccababbdbee1ff76f2cb51d81c4781b81aac2c
Ishikki Akabane <rohansjaishwal@gmail.com>
2022-12-26 19:30:48 +0530

    Update notes_sql.py

--- RUKA/modules/sql/notes_sql.py
+++ RUKA/modules/sql/notes_sql.py
@@ -1,8 +1,8 @@
 # Note: chat_id's are stored as strings because the int is too large to be stored in a PSQL database.
 import threading
 
-from SUMI.modules.helper_funcs.msg_types import Types
-from SUMI.modules.sql import BASE, SESSION
+from RUKA.modules.helper_funcs.msg_types import Types
+from RUKA.modules.sql import BASE, SESSION
 from sqlalchemy import (Boolean, Column, Integer, String, UnicodeText, distinct,
                         func)
 
