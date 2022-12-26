a7c821782145347315411ef08f6db3b5f7fe18b3
Ishikki Akabane <rohansjaishwal@gmail.com>
2022-12-26 19:11:58 +0530

    Update telegraph.py

--- RUKA/modules/telegraph.py
+++ RUKA/modules/telegraph.py
@@ -1,12 +1,12 @@
-from SUMI.events import register
-from SUMI import telethn as tbot
+from RUKA.events import register
+from RUKA import telethn as tbot
 TMP_DOWNLOAD_DIRECTORY = "./"
 from telethon import events
 import os
 from PIL import Image
 from datetime import datetime
 from telegraph import Telegraph, upload_file, exceptions
-Anonymous = "SUMI"
+Anonymous = "RUKA"
 telegraph = Telegraph()
 r = telegraph.create_account(short_name=Anonymous)
 auth_url = r["auth_url"]
