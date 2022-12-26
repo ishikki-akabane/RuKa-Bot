b0b8fa80ed03531db89e9a53cb8377441d539c1b
Ishikki Akabane <rohansjaishwal@gmail.com>
2022-12-26 19:17:20 +0530

    Update filters.py

--- RUKA/modules/helper_funcs/filters.py
+++ RUKA/modules/helper_funcs/filters.py
@@ -1,7 +1,7 @@
 from telegram import Message
 from telegram.ext import MessageFilter
 
-from SUMI import DEMONS, DRAGONS, DEV_USERS
+from RUKA import DEMONS, DRAGONS, DEV_USERS
 
 
 class CustomFilters(object):
