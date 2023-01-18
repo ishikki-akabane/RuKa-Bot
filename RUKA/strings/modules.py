dd6135ea5ca4682fdefa57c8d59b3cad4ba225bd
Ishikki Akabane <rohansjaishwal@gmail.com>
2023-01-18 17:49:51 +0530

    Create modules.py

---
+++ RUKA/strings/modules.py
@@ -0,0 +1,23 @@
+ADMIN_STR = """
+ ❍ /admins*:* list of admins in the chat
+
+*Admins only:*
+ ❍ /pin*:* silently pins the message replied to - add `'loud'` or `'notify'` to give notifs to users
+ ❍ /unpin*:* unpins the currently pinned message
+ ❍ /invitelink*:* gets invitelink
+ ❍ /fullpromote*:* promotes the user with all rights
+ ❍ /promote*:* promotes the user with basic rights
+ ❍ /demote*:* demotes the user
+ ❍ /title <title here>*:* sets a custom title for an admin that the bot promoted
+ ❍ /setgtitle <newtitle>*:* Sets new chat title in your group.
+ ❍ /setgpic*:* As a reply to file or photo to set group profile pic!
+ ❍ /delgpic*:* Same as above but to remove group profile pic.
+ ❍ /setsticker*:* As a reply to some sticker to set it as group sticker set!
+ ❍ /setdescription <description>*:* Sets new chat description in group.
+ ❍ /admincache*:* force refresh the admins list
+ ❍ /del*:* deletes the message you replied to
+ ❍ /purge*:* deletes all messages between this and the replied to message.
+ ❍ /purge <integer X>*:* deletes the replied message, and X messages following it if replied to a message.
+ """
+
+ANIME_STR = """
