7ae0902b0d84ce68e1165e7dece7a3b1ebfcaa30
Ishikki Akabane <rohansjaishwal@gmail.com>
2023-01-18 17:49:48 +0530

    Create main.py

---
+++ RUKA/strings/main.py
@@ -0,0 +1,12 @@
+PM_START_MSG = """
+*Konichiwa ,*
+*I'm RuKa SaRaShiNa, I'm A Powerful Group Management Bot.*
+❍ *Owner - @IshikkiAkabane*
+❍ *Uptime* - {}
+❍ *Users* - {}
+❍ *Chats* - {}
+**ᴄʟɪᴄᴋ ᴛʜᴇ ʜᴇʟᴘ ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ ꜰᴏʀ ᴍᴏʀᴇ.**
+"""
+GROUP_START_MSG = """
+
+"""
