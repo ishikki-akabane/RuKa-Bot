1a71b279c56f494d0d0f0fd10d6437100c4f82de
Ishikki Akabane <rohansjaishwal@gmail.com>
2023-01-19 11:59:49 +0530

    Update __init__.py

---
+++ RUKA/__init__.py
@@ -0,0 +1,39 @@
+import logging
+import os
+import platform
+import sys
+import time
+import telegram.ext as tg
+import random
+import asyncio
+
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
+
+# Enable Logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
+    level=logging.INFO,
+)
+LOGGER = logging.getLogger(__name__)
+
+
+# Python version must be above or equal to 3.7
+if sys.version_info[0] < 3 or sys.version_info[1] < 6:
+    LOGGER.error(
+        "You MUST have a python version of at least 3.9! Multiple features depend on this. Bot quitting.",
+    )
+    quit(1)
+
+
+# Starts the clock to note since how long the bot is running
+StartTime = time.time()
+
+
+TOKEN = ""
+
+
+application = ApplicationBuilder().token(TOKEN).post_init(post_init).build()
+asyncio.get_event_loop().run_until_complete(application.bot.initialize())
