536cf0e2411b04bf4265ee98e8d0d58cd73e760b
Ishikki Akabane <rohansjaishwal@gmail.com>
2023-04-07 17:30:15 +0530

    Create __init__.py

---
+++ RUKA/database/sql/__init__.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+from RUKA import DB_URI
+
+MAX_POOL_SIZE = 5
+
+engine = create_engine(DB_URI, pool_size=MAX_POOL_SIZE, max_overflow=0)
+BASE = declarative_base()
+Session = scoped_session(sessionmaker(bind=engine))
+
+# Define your models here
+
+def start():
+    BASE.metadata.create_all(engine)
+    return Session()
+
+# Use the session to query your database
+session = start()
+result = session.query(User).filter(User.id == 1).one()
