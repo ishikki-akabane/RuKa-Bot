b79611974699d3eaa971397b8a60dc5e1e799e79
Ishikki Akabane <rohansjaishwal@gmail.com>
2023-04-07 16:02:06 +0530

    Delete main.py

--- RUKA/strings/main.py
+++
@@ -1,7 +0,0 @@
-PM_START_MSG = """
-*Konichiwa ,*
-*I'm RuKa SaRaShiNa, I'm A Powerful Group Management Bot.*
-❍ *Owner - @IshikkiAkabane*
-❍ *Uptime* - {}
-**ᴄʟɪᴄᴋ ᴛʜᴇ ʜᴇʟᴘ ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ ꜰᴏʀ ᴍᴏʀᴇ.**
-"""
