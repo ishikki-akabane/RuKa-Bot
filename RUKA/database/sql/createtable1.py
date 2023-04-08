402bbf00d699751aafeaf0587ca43538051201e2
Ishikki Akabane <rohansjaishwal@gmail.com>
2023-04-08 16:02:56 +0530

    Update createtable1.py

--- RUKA/database/sql/createtable1.py
+++ RUKA/database/sql/createtable1.py
@@ -3,16 +3,13 @@ from RUKA.database.sql import SQLDB
 import asyncio
 
 
-user_table_query = '''
+CREATE_USER_TABLE = '''
         CREATE TABLE IF NOT EXISTS user_table (
             user_id BIGINT PRIMARY KEY,
             name VARCHAR(255),
         )
 '''
 
-async def create_table1():
-    r1 = await SQLDB(user_table_query, commit=True)
-    return True
 
 
-asyncio.run(create_table1())
+#asyncio.run(create_table1())
