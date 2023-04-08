8799fc927689c1638c2a11c09c5e7566e5e0a737
Ishikki Akabane <rohansjaishwal@gmail.com>
2023-04-08 15:35:58 +0530

    Create createtable1.py

---
+++ RUKA/database/sql/createtable1.py
@@ -0,0 +1,14 @@
+#FIRST
+from RUKA.database.sql import SQLDB
+
+
+user_table_query = '''
+        CREATE TABLE IF NOT EXISTS user_table (
+            user_id BIGINT PRIMARY KEY,
+            name VARCHAR(255),
+        )
+'''
+
+async def create_table1():
+    r1 = await SQLDB(user_table_query, commit=True)
+    return True
