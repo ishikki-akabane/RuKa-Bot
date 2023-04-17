1d045081d903a85f27525b83d1a68f247ef53846
Ishikki Akabane <rohansjaishwal@gmail.com>
2023-04-17 11:49:52 +0530

    Update admin_funcs.py

--- RUKA/helpers/admin_funcs.py
+++ RUKA/helpers/admin_funcs.py
@@ -1,4 +1,5 @@
 from functools import wraps
+import random
 
 from telegram import Update
 from telegram.ext import ContextTypes
@@ -6,35 +7,32 @@ from telegram.ext import ContextTypes
 """FUN Lines""" # designed by ishikki akabane(t.me/ishikki_akabane)
 funny_lines = {
     "can_add_web_page_previews": [
-        "Sorry, you're not allowed to share links in this group. Looks like someone's not ready for the World Wide Web yet!"
+        "ꜱᴏʀʀʏ, ʏᴏᴜ'ʀᴇ ɴᴏᴛ ᴀʟʟᴏᴡᴇᴅ ᴛᴏ ꜱʜᴀʀᴇ ʟɪɴᴋꜱ ɪɴ ᴛʜɪꜱ ɢʀᴏᴜᴘ. ʟᴏᴏᴋꜱ ʟɪᴋᴇ ꜱᴏᴍᴇᴏɴᴇ'ꜱ ɴᴏᴛ ʀᴇᴀᴅʏ ꜰᴏʀ ᴛʜᴇ ᴡᴏʀʟᴅ ᴡɪᴅᴇ ᴡᴇʙ ʏᴇᴛ!"
     ],
     "can_send_polls": [
-        "",
-        ""
+        "ꜱᴏʀʀʏ, ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴛʜᴇ ʀɪɢʜᴛ ᴛᴏ ꜱᴛᴀʀᴛ ᴀ ᴘᴏʟʟ. ɪ ɢᴜᴇꜱꜱ ʏᴏᴜʀ ᴏᴘɪɴɪᴏɴꜱ ᴀʀᴇɴ'ᴛ ᴛʜᴀᴛ ᴘᴏᴘᴜʟᴀʀ!"
     ],
     "can_change_info": [
-
-    ],
-    "can_post_messages": [
-
+        "ꜱᴏʀʀʏ, ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴛʜᴇ ʀɪɢʜᴛ ᴛᴏ ᴇᴅɪᴛ ɢʀᴏᴜᴘ ɪɴꜰᴏ. ɪ ɢᴜᴇꜱꜱ ᴛʜᴇ ɢʀᴏᴜᴘ ɴᴀᴍᴇ ᴡɪʟʟ ʜᴀᴠᴇ ᴛᴏ ꜱᴛᴀʏ ʙᴏʀɪɴɢ ꜰᴏʀ ɴᴏᴡ!"
     ],
     "can_delete_messages": [
 
     ],
-    "can_edit_messages": [
-
-    ],
     "can_invite_users": [
-
+        "ꜱᴏʀʀʏ, ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴛʜᴇ ʀɪɢʜᴛ ᴛᴏ ɪɴᴠɪᴛᴇ ᴜꜱᴇʀꜱ ᴛᴏ ᴛʜɪꜱ ɢʀᴏᴜᴘ. ᴅɪᴅ ʏᴏᴜ ꜰᴏʀɢᴇᴛ ʏᴏᴜʀ ꜱᴇᴄʀᴇᴛ ʜᴀɴᴅꜱʜᴀᴋᴇ?",
+        "ꜱᴏʀʀʏ, ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴛʜᴇ ʀɪɢʜᴛ ᴛᴏ ɪɴᴠɪᴛᴇ ᴜꜱᴇʀꜱ ᴛᴏ ᴛʜɪꜱ ɢʀᴏᴜᴘ. ᴍᴀʏʙᴇ ᴛʜᴇʏ ᴊᴜꜱᴛ ᴅᴏɴ'ᴛ ʟɪᴋᴇ ʏᴏᴜ ᴇɴᴏᴜɢʜ ʏᴇᴛ!"   
     ],
     "can_restrict_members": [
-
+        "ꜱᴏʀʀʏ, ʏᴏᴜ'ʀᴇ ɴᴏᴛ ᴡᴏʀᴛʜʏ ᴛᴏ ᴡɪᴇʟᴅ ᴛʜᴇ ʙᴀɴ ʜᴀᴍᴍᴇʀ. ᴅɪᴅ ʏᴏᴜ ꜰᴏʀɢᴇᴛ ʏᴏᴜʀ ꜱᴜᴘᴇʀʜᴇʀᴏ ᴄᴀᴘᴇ ᴀᴛ ʜᴏᴍᴇ?",
+        "ꜱᴏʀʀʏ, ʏᴏᴜ'ʀᴇ ɴᴏᴛ ᴡᴏʀᴛʜʏ ᴛᴏ ᴡɪᴇʟᴅ ᴛʜᴇ ʙᴀɴ ʜᴀᴍᴍᴇʀ. ɪᴛ'ꜱ ᴏᴋᴀʏ, ᴡᴇ'ʟʟ ʜᴀɴᴅʟᴇ ᴛʜᴇ ᴛᴏᴜɢʜ ᴅᴇᴄɪꜱɪᴏɴꜱ ꜰᴏʀ ʏᴏᴜ!"   
     ],
     "can_pin_messages": [
-
+        "ꜱᴏʀʀʏ, ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴛʜᴇ ʀɪɢʜᴛ ᴛᴏ ᴘɪɴ ᴍᴇꜱꜱᴀɢᴇꜱ ɪɴ ᴛʜɪꜱ ɢʀᴏᴜᴘ. ɪ ɢᴜᴇꜱꜱ ʏᴏᴜʀ ᴍᴇꜱꜱᴀɢᴇꜱ ᴊᴜꜱᴛ ᴀʀᴇɴ'ᴛ ꜱᴛɪᴄᴋʏ ᴇɴᴏᴜɢʜ!",
+        "ꜱᴏʀʀʏ, ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴛʜᴇ ʀɪɢʜᴛ ᴛᴏ ᴘɪɴ ᴍᴇꜱꜱᴀɢᴇꜱ ɪɴ ᴛʜɪꜱ ɢʀᴏᴜᴘ. ɪ ɢᴜᴇꜱꜱ ʏᴏᴜʀ ᴊᴏᴋᴇꜱ ᴀʀᴇɴ'ᴛ ꜰᴜɴɴʏ ᴇɴᴏᴜɢʜ ᴛᴏ ꜱᴛɪᴄᴋ ᴀʀᴏᴜɴᴅ!"
     ],
     "can_promote_members": [
-
+        "ꜱᴏʀʀʏ, ʏᴏᴜ'ʀᴇ ɴᴏᴛ ᴡᴏʀᴛʜʏ ᴏꜰ ᴛʜᴇ ᴘʀᴏᴍᴏᴛɪᴏɴ. ᴍᴀʏʙᴇ ʏᴏᴜ ɴᴇᴇᴅ ᴛᴏ ᴡᴏʀᴋ ᴏɴ ʏᴏᴜʀ ʙʀᴏᴡɴ-ɴᴏꜱɪɴɢ ꜱᴋɪʟʟꜱ!",
+        "ꜱᴏʀʀʏ, ʏᴏᴜ'ʀᴇ ɴᴏᴛ ᴡᴏʀᴛʜʏ ᴏꜰ ᴛʜᴇ ᴘʀᴏᴍᴏᴛɪᴏɴ. ᴍᴀʏʙᴇ ɪꜰ ʏᴏᴜ ᴡᴇʀᴇɴ'ᴛ ᴀʟᴡᴀʏꜱ ʟᴀᴛᴇ ᴛᴏ ᴍᴇᴇᴛɪɴɢꜱ..."
     ],
     "can_manage_chat": [
 
@@ -86,7 +84,7 @@ def check_admin(
                     elif permission == "can_restrict_members":
                         message = "Sorry, you're not worthy to wield the ban hammer. Did you forget your superhero cape at home?"
                     else:
-                        message = "Sorry, you don't have permission to perform this action. Looks like someone's stuck in the kiddie pool!"
+                        message = "ꜱᴏʀʀʏ, ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴘᴇʀᴍɪꜱꜱɪᴏɴ ᴛᴏ ᴘᴇʀꜰᴏʀᴍ ᴛʜɪꜱ ᴀᴄᴛɪᴏɴ. ʟᴏᴏᴋꜱ ʟɪᴋᴇ ꜱᴏᴍᴇᴏɴᴇ'ꜱ ꜱᴛᴜᴄᴋ ɪɴ ᴛʜᴇ ᴋɪᴅᴅɪᴇ ᴘᴏᴏʟ!"
                         
                     await message.reply_text(message)
                 
