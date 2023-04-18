9bb03ba9a4d94d85e976e67ef3e3a006d91c5499
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2023-04-18 20:45:13 +0530

    Update rank_help.py

--- RUKA/helpers/rank_help.py
+++ RUKA/helpers/rank_help.py
@@ -7,24 +7,24 @@ from RUKA import DEV_USERS, SUDO_USERS, SUPPORT_USERS, WHITE_LIST, OWNER_ID, LOG
 def status(rank):
     def decorator(func):
         @wraps(func)
-        async def wrapper(update: Update, context: ContextTypes.DEFAULT_TYPE):
+        def wrapper(update: Update, context: ContextTypes.DEFAULT_TYPE):
             user_id = update.effective_user.id
             message = update.effective_message
 
             if rank == 'dev' and user_id not in DEV_USERS:
-                await message.reply_text("Only developers of the bot can use this command")
+                message.reply_text("Only developers of the bot can use this command")
                 return
             elif rank == 'sudo' and user_id not in SUDO_USERS:
-                await message.reply_text("Only user with sudo access can use this command")
+                message.reply_text("Only user with sudo access can use this command")
                 return
             elif rank == 'support' and user_id not in SUPPORT_USERS:
-                await message.reply_text("You not worthy to use this command")
+                message.reply_text("You not worthy to use this command")
                 return
             elif rank == 'white' and user_id not in WHITE_LIST:
-                await message.reply_text("Only for special ranks user!!")
+                message.reply_text("Only for special ranks user!!")
                 return
             elif rank == 'owner' and user_id != OWNER_ID:
-                await message.reply_text("Only my dear owner can you use this!!")
+                message.reply_text("Only my dear owner can you use this!!")
                 return
 
             return func(update, context)
