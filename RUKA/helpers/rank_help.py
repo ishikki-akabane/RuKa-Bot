12f793a1863702866de4c965c3f0c09c7ba41db1
Ishikki Akabane <rohansjaishwal@gmail.com>
2023-04-24 13:49:36 +0530

    Update rank_help.py

--- RUKA/helpers/rank_help.py
+++ RUKA/helpers/rank_help.py
@@ -3,6 +3,9 @@ from telegram import Update
 from telegram.ext import ContextTypes
 from RUKA import DEV_USERS, SUDO_USERS, SUPPORT_USERS, WHITE_LIST, OWNER_ID, LOGGER
 
+WHITE_LIST = DEV_USERS + SUDO_USERS + SUPPORT_USERS + WHITE_LIST
+SUPPORT_LIST = DEV_USERS + SUDO_USERS + SUPPORT_USERS
+SUDO_LIST = DEV_USERS + SUDO_USERS
 
 def status(rank):
     def decorator(func):
@@ -14,10 +17,10 @@ def status(rank):
             if rank == 'dev' and user_id not in DEV_USERS:
                 await message.reply_text("Only developers of the bot can use this command")
                 return
-            elif rank == 'sudo' and user_id not in SUDO_USERS:
+            elif rank == 'sudo' and user_id not in SUDO_LIST:
                 await message.reply_text("Only user with sudo access can use this command")
                 return
-            elif rank == 'support' and user_id not in SUPPORT_USERS:
+            elif rank == 'support' and user_id not in SUPPORT_LIST:
                 await message.reply_text("You not worthy to use this command")
                 return
             elif rank == 'white' and user_id not in WHITE_LIST:
