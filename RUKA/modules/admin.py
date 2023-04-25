5a3bee26334f0e56f63dcc265821ac68336a7b27
Ishikki Akabane <rohansjaishwal@gmail.com>
2023-04-25 20:34:06 +0530

    Delete admin.py

--- RUKA/modules/admin.py
+++
@@ -1,9 +0,0 @@
-import random
-import html
-from datetime import datetime
-
-from RUKA import dp
-
-from telegram import Update
-from telegram.ext import ContextTypes, CommandHandler
-from telegram.constants import ParseMode
