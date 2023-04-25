347b0260f78d51dfd99738b37a3630967b50e214
Ishikki Akabane <rohansjaishwal@gmail.com>
2023-04-25 20:33:37 +0530

    Create admin.py

---
+++ RUKA/modules/admin.py
@@ -0,0 +1,9 @@
+import random
+import html
+from datetime import datetime
+
+from RUKA import dp
+
+from telegram import Update
+from telegram.ext import ContextTypes, CommandHandler
+from telegram.constants import ParseMode
