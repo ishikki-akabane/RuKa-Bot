23e387bdf730ae5c7bf5fceff11b0cec0f05dd0e
Sugar <108065240+sugar-Ik@users.noreply.github.com>
2023-05-07 14:54:21 +0530

    Update rank_help.py

--- RUKA/helpers/rank_help.py
+++ RUKA/helpers/rank_help.py
@@ -1,11 +1,8 @@
 from functools import wraps
 from telegram import Update
 from telegram.ext import ContextTypes
-from RUKA import DEV_USERS, SUDO_USERS, SUPPORT_USERS, WHITE_USERS, OWNER_ID, LOGGER
+from RUKA.helpers import DEVLIST, SUDOLIST, SUPPORTLIST, WHITELIST, OWNER_ID, LOGGER
 
-WHITE_LIST = DEV_USERS + SUDO_USERS + SUPPORT_USERS + WHITE_USERS
-SUPPORT_LIST = DEV_USERS + SUDO_USERS + SUPPORT_USERS
-SUDO_LIST = DEV_USERS + SUDO_USERS
 
 def status(rank):
     def decorator(func):
@@ -14,16 +11,16 @@ def status(rank):
             user_id = update.effective_user.id
             message = update.effective_message
 
-            if rank == 'dev' and user_id not in DEV_USERS:
+            if rank == 'dev' and user_id not in DEVLIST:
                 await message.reply_text("Only developers of the bot can use this command")
                 return
-            elif rank == 'sudo' and user_id not in SUDO_LIST:
+            elif rank == 'sudo' and user_id not in SUDOLIST:
                 await message.reply_text("Only user with sudo access can use this command")
                 return
-            elif rank == 'support' and user_id not in SUPPORT_LIST:
+            elif rank == 'support' and user_id not in SUPPORTLIST:
                 await message.reply_text("You not worthy to use this command")
                 return
-            elif rank == 'white' and user_id not in WHITE_LIST:
+            elif rank == 'white' and user_id not in WHITELIST:
                 await message.reply_text("Only for special ranks user!!")
                 return
             elif rank == 'owner' and user_id != OWNER_ID:
