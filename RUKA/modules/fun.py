b285cae1ea73e774dafad3750cdd8c58dcf64807
Sugar <108065240+sugar-Ik@users.noreply.github.com>
2023-05-08 18:19:56 +0530

    Update fun.py

--- RUKA/modules/fun.py
+++ RUKA/modules/fun.py
@@ -1,7 +1,10 @@
-import json
-import aiohttp
+"""
+This whole module is created by ishikki akabane, dont kang, please give credits. As a developer, you should always give respect to others hardwork in coding.
+Thankyou :) , have code-full day for you.
+"""
+import time
 
-from RUKA import dp
+from RUKA import dp, OWNER_ID
 from RUKA.helpers.extra import mention
 from RUKA.helpers.requests import bluerequest
 
@@ -11,7 +14,16 @@ from telegram.constants import ParseMode
 
 
 baseblue_url = "https://blue-api.vercel.app"
-
+"""
+    slap_lines = [
+        f"{user1} just slapped {user2} with a wet noodle! It's super effective!",
+        f"{user1} gave {user2} a gentle slap. They're probably reconsidering their life choices now.",
+        f"{user1} delivered a powerful slap to {user2}'s face. The crowd goes wild!",
+        f"{user1} slapped {user2} so hard, their ancestors felt it!",
+        f"{user1} playfully slapped {user2} on the cheek. Friendship level: ouch!",
+        f"{user1} unleashed a mighty slap on {user2}! The shockwaves could be felt for miles.",
+    ]
+"""
 
 async def slap(update: Update, context: ContextTypes.DEFAULT_TYPE):
     bot, args = context.bot, context.args
@@ -29,17 +41,29 @@ async def slap(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await message.reply_text("can i slap you please :)")
         return
 
-    attacker = update.effective_user.id # user id of the user who is slapping
-    defender = message.reply_to_message.from_user.id
+    attacker = update.effective_user # user who is slapping
+    defender = message.reply_to_message.from_user # user who is going to get slapped
     
-    if attacker == defender:
+    if attacker.id == defender.id: # same persons
         await message.reply_text("You want to slap yourself ?")
         return
-    
-    url = baseblue_url + "/slap"
-    response = await bluerequest(url)
-    print(response)
-    await message.reply_animation(response["msg"])
+    elif defender.id == bot.id: # slaping the bot
+        await bot.restrict_chat_member(
+            chat.id,
+            attacker.id,
+            until_date=time.time() + 60
+        )
+        await message.reply_text("I will mute you for slapping me you perve")
+        return 
+    elif defender.id == OWNER_ID:
+        await message.reply_text("bruh, i cant slap my love :/")
+        return
+    else:
+        url = baseblue_url + "/slap"
+        response = await bluerequest(url)
+        user1 = mention(attacker.id, attacker.first_name, mention=True)
+        user2 = mention(defender.id, defender.first_name, mention=True)
+        await message.reply_animation(response["msg"], caption=f"{user1} slapped {user2} so hard, their ancestors felt it", parsemode=ParseMode.MARKDOWN)
 
 
 dp.add_handler(CommandHandler("slap", slap))
