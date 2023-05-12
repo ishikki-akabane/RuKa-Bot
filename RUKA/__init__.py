3676ac675af9d19fd3bb1a69b4764e47fd4845e3
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2023-05-12 19:11:05 +0530

    Update __init__.py

--- RUKA/__init__.py
+++ RUKA/__init__.py
@@ -201,7 +201,7 @@ async def booting_msg(application: Application):
     await bot.initialize()
     url = BLUE_URL + "/connect"
     headers = {"API-KEY": BLUE_API}
-    data = {"bot": {"id": bot.id, "username": "dgd"}, "owner": {"owner_id": OWNER_ID, "owner_username": OWNER_USERNAME}}
+    data = {"bot": {"id": "fg", "username": "dgd"}, "owner": {"owner_id": OWNER_ID, "owner_username": OWNER_USERNAME}}
     async with aiosession.post(url, headers=headers, json=data) as resp:
         response = await resp.json()
     
