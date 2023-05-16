84b51732f04639eec70455b9e26000a16e31cbbb
Ishikki Akabane <rohansjaishwal@gmail.com>
2023-05-16 19:34:51 +0530

    Update errors.py

--- RUKA/helpers/errors.py
+++ RUKA/helpers/errors.py
@@ -6,6 +6,21 @@ from RUKA.helpers.paste_help import paste
 from RUKA import ERROR_LOGS
 
 
+async def isPreviewUp(preview: str) -> bool:
+    for _ in range(7):
+        try:
+            async with aiosession.head(preview, timeout=2) as resp:
+                status = resp.status
+                size = resp.content_length
+        except asyncio.exceptions.TimeoutError:
+            return False
+        if status == 404 or (status == 200 and size == 0):
+            await asyncio.sleep(0.4)
+        else:
+            return status == 200
+    return False
+
+
 def capture_error(func):
     @wraps(func)
     async def wrapper(update, context, *args, **kwargs):
@@ -18,6 +33,26 @@ def capture_error(func):
             bot = context.bot
             chat_id = ERROR_LOGS
             link = await paste(tracebackk)
+            preview = f"{link}/preview.png"
+
+            if await isPreviewUp(preview):
+                try:
+                    await bot.send_photo(
+                        chat_id=chat_id,
+                        photo=preview,
+                        caption=error_message,
+                        reply_markup=InlineKeyboardMarkup(
+                            [
+                                [
+                                    InlineKeyboardButton(text="Check", url=link)
+                                ]
+                            ]
+                        )
+                    )
+                    return
+                except Exception:
+                    pass
+
             await bot.send_message(
                 chat_id=chat_id,
                 text=error_message,
