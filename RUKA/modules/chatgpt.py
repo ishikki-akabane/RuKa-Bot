837043fe8fa9033dafce7f07f12c31ca2c8af4c8
Ishikki Akabane <rohansjaishwal@gmail.com>
2023-05-21 12:30:03 +0530

    Update chatgpt.py

--- RUKA/modules/chatgpt.py
+++ RUKA/modules/chatgpt.py
@@ -13,21 +13,17 @@ async def chatgpt(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message = update.effective_message
     text = message.text
     bot = context.bot
-    user_id = update.effective_user.id
     
-    reply = message.reply_to_message
-    target_id = reply.from_user.id
-    if target_id == 6208314828:
-        if text is not None:
-            await bot.send_chat_action(chat_id=update.effective_chat.id, action='typing')
+    if text is not None:
+        await bot.send_chat_action(chat_id=update.effective_chat.id, action='typing')
 
-            url = BLUE_URL + "/chatgpt"
-            data = {"param": {"message": text, "chat_mode": "code_assistant"}}
+        url = BLUE_URL + "/chatgpt"
+        data = {"param": {"message": text, "chat_mode": "code_assistant"}}
 
-            query = await bluerequest(url, data=data)
-            msg = query["msg"]
+        query = await bluerequest(url, data=data)
+        msg = query["msg"]
             
-            await message.reply_text(f"{msg}", parse_mode=ParseMode.MARKDOWN)
+        await message.reply_text(f"{msg}", parse_mode=ParseMode.MARKDOWN)
 
 
 
