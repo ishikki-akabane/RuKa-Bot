3905e6cc5dea05a7ac3b9e1a903c19c640c51232
Ishikki Akabane <rohansjaishwal@gmail.com>
2023-05-26 18:55:39 +0530

    Create chatbot_sql.py

---
+++ RUKA/database/sql/chatbot_sql.py
@@ -0,0 +1,33 @@
+from RUKA.database.sql import SQLDB
+
+
+async def sql_addchatbot(chat_id, version):
+    chat_id = int(chat_id)
+    query = '''INSERT INTO chatbot_table (chat_id, version)
+            VALUES ({}, {}, '{}')
+    '''
+    result = await SQLDB(query.format(chat_id, version), commit=True)
+    return result
+
+
+async def sql_removechatbot(chat_id):
+    chat_id = int(chat_id)
+    query = '''DELETE FROM chatbot_table WHERE chat_id = {}'''
+    result = await SQLDB(query.format(chat_id), commit=True)
+    return result
+
+
+async def sql_updatechatbot(chat_id, version):
+    chat_id = int(chat_id)
+    query = '''UPDATE chatbot_table SET version = {} WHERE chat_id = {}'''
+    result = await SQLDB(query.format(chat_id, version), commit=True)
+    return result
+
+
+async def checkchat(chat_id):
+    chat_id = int(chat_id)
+    query = '''SELECT * FROM chatbot_table WHERE chat_id = {}'''
+    result = await SQLDB(query.format(chat_id, version))
+    print(result)
+    return None
+    
