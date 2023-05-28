dd98e1535501a35c69d19d3696189c9049633d70
Ishikki Akabane <rohansjaishwal@gmail.com>
2023-05-28 17:56:31 +0530

    Update createtable1.py

--- RUKA/database/sql/createtable1.py
+++ RUKA/database/sql/createtable1.py
@@ -4,7 +4,7 @@ CREATE_AFK_TABLE = '''
     CREATE TABLE IF NOT EXISTS afk_table (
         user_id BIGINT,
         reason VARCHAR(255),
-        date dat
+        date date
     )
 '''
 
@@ -12,5 +12,13 @@ CREATE_CHATBOT_TABLE = '''
     CREATE TABLE IF NOT EXISTS chatbot_table (
         chat_id BIGINT,
         version INTEGER
+    )
+'''
+
+CREATE_GBAN_TABLE = '''
+    CREATE TABLE IF NOT EXISTS gban_table (
+        user_id BIGINT,
+        name VARCHAR(255),
+        reason VARCHAR(255)
     )
 '''
