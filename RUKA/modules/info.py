4606f452db576e6e5caf9467833d0b98c12745c3
Ishikki Akabane <rohansjaishwal@gmail.com>
2023-05-31 19:04:29 +0530

    Update info.py

--- RUKA/modules/info.py
+++ RUKA/modules/info.py
@@ -3,7 +3,7 @@ from RUKA import dp, BLUE_URL
 from RUKA.helpers.errors import capture_error
 from RUKA.helpers.requests import bluerequest
 from RUKA.helpers.rank_help import status
-from RUKA.database.sql.chatbot_sql import sql_addchatbot, sql_removechatbot, sql_updatechatbot, checkchat, chatbot_list
+from RUKA.database.sql.user_sql import sql_get_userid
 
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ContextTypes, CommandHandler, MessageHandler, filters, CallbackQueryHandler
@@ -21,25 +21,48 @@ async def info(update: Update, context: ContextTypes.DEFAULT_TYPE):
     reply = message.reply_to_message
     if reply:
         target = reply.from_user
+        target_id = target.id
     else:
         if len(args) > 0:
             if args[0][0] == "@":
                 user_name = args[0]
                 user_name = user_name[1:]
-                target = await bot.get_chat(user_name)
-                await message.reply_text(f"{target.id}")
-                return
+                try:
+                    target_id = await sql_get_userid(user_name)
+                except:
+                    return await message.reply_text("User not found :(")
+            else:
+                try:
+                    target_id = args[0]
+                    target_id = int(target_id)
+                except:
+                    return await message.reply_text("User id is not valid!!")
 
             try:
-                target_id = args[0]
-                target_id = int(target_id)
                 target = await bot.get_chat(target_id)
-                await message.reply_text(f"{target.first_name}")
-            except Exception as e:
-                print(e)
-                return await message.reply_text("User id is not valid!!")
+            except:
+                return await message.reply_text("User not found :(")        
         else:
             return await message.reply_text("Bro wtf, you havent provided me anything :/")
+    
+    first_name =  target.first_name
+    if target.last_name:
+        last_name = target.last_name
+    else:
+        last_name = None
+    user_name = target.username
+    info_msg = f"""
+---------------
+userID: {target_id}
+firstname: {first_name}
+lastname: {last_name}
+username: {username}
+---------------
+"""
+    await message.reply_text(
+        info_msg,
+        parse_mode=ParseMode.MARKDOWN
+    )
 
         
 dp.add_handler(CommandHandler("infoo", info, block=False))
