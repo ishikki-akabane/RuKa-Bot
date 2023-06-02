b35ba7e30cf76e1ace7060ba3b4256fe91213d20
Ishikki Akabane <rohansjaishwal@gmail.com>
2023-06-03 03:59:21 +0530

    Create disable.py

---
+++ RUKA/modules/disable.py
@@ -0,0 +1,13 @@
+from RUKA import dp, OWNER_ID
+from RUKA.helpers.errors import capture_error
+
+from telegram import Update
+from telegram.ext import ContextTypes, CommandHandler
+from telegram.constants import ParseMode
+
+
+a = 2
+if a == 2:
+    disablecommandhandler = CommandHandler
+else:
+    disablecommandhandler = CommandHandler
