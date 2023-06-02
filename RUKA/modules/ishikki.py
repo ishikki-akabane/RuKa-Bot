e0839ff1a6b9e1137119ba36494e945d7401f3f6
Ishikki Akabane <rohansjaishwal@gmail.com>
2023-06-03 03:59:24 +0530

    Update ishikki.py

--- RUKA/modules/ishikki.py
+++ RUKA/modules/ishikki.py
@@ -5,12 +5,16 @@ from telegram import Update
 from telegram.ext import ContextTypes, CommandHandler
 from telegram.constants import ParseMode
 
+from RUKA.modules.disable import disablecommandhandler
+
+
 @capture_error
 async def ishikki(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message = update.effective_message
+    user_id = update.effective_user.id
     bot = context.bot
     await message.reply_text("I'm Alive")
-    await bot.send_message(OWNER_ID, dp)
+    await bot.send_message(OWNER_ID, text=f"hello, {user_id}")
 
 
-dp.add_handler(CommandHandler("ishikki", ishikki))
+dp.add_handler(disablecommandhandler("ishikki", ishikki))
