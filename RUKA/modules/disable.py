72c6d0099c68c79a08b3efa82678853706daacbc
Ishikki Akabane <rohansjaishwal@gmail.com>
2023-06-03 05:34:34 +0530

    Update disable.py

--- RUKA/modules/disable.py
+++ RUKA/modules/disable.py
@@ -1,4 +1,4 @@
-from RUKA import dp, OWNER_ID
+from RUKA import dp, OWNER_ID, LOGGER
 from RUKA.helpers.errors import capture_error
 
 from telegram import Update
@@ -6,9 +6,25 @@ from telegram.ext import ContextTypes, CommandHandler
 from telegram.constants import ParseMode
 
 
+DISABLE_CMDS = []
+
 a = 2
 if a == 2:
-    disablecommandhandler = CommandHandler
+    class disablecommandhandler(CommandHandler):
+        def __init__(self, command, callback, block: bool, filters: filters_module.BaseFilter = None):
+
+            if type(command) == list:
+                for comnd in command:
+                    comnd = comnd.lower()
+                    DISABLE_CMDS.append(comnd)
+            elif type(command) == str:
+                command = command.lower()
+                DISABLE_CMDS.append(command)
+            else:
+                LOGGER.error(f"Command: {command} is not a valid bot command")
+
+            print("done")
+
 else:
     disablecommandhandler = CommandHandler
 
