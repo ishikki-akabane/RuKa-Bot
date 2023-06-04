c4d20c4ac0db07253a2e95313f9bc7e152f2d091
Ishikki Akabane <rohansjaishwal@gmail.com>
2023-06-04 14:21:48 +0530

    Update disable.py

--- RUKA/modules/disable.py
+++ RUKA/modules/disable.py
@@ -11,15 +11,29 @@ DISABLE_CMDS = []
 
 a = 2
 if a == 2:
-    class disablecommandhandler(CommandHandler):
+    class DisableCommandHandler(CommandHandler):
         def __init__(self, command, callback, block = True, filters: filters_module.BaseFilter = None):
             super().__init__(command, callback, block=block)
+            
+            if type(command) == list:
+                for comnd in command:
+                    comnd = comnd.lower()
+                    DISABLE_CMDS.append(comnd)
+            elif type(command) == str:
+                command = command.lower()
+                DISABLE_CMDS.append(command)
+            else:
+                LOGGER.error(f"Command: {command} is not a valid bot command")
+
+            print("done")
+            print(DISABLE_CMDS)
 
     def check_update(self, update):
         chat_id = update.effective_chat.id
         #if chat_id in disabled_commands and disabled_commands[chat_id] == self.command[0]:
         #    return False
         return super().check_update(update)
+
     """
     class disablecommandhandler(CommandHandler):
         def __init__(self, command, callback, block = True, filters: filters_module.BaseFilter = None):
