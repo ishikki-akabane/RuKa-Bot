e73842fbfad5a19a760251d449b912adbce91047
Ishikki Akabane <rohansjaishwal@gmail.com>
2023-06-04 14:52:53 +0530

    Update disable.py

--- RUKA/modules/disable.py
+++ RUKA/modules/disable.py
@@ -6,8 +6,11 @@ from telegram.ext import ContextTypes, CommandHandler
 from telegram.constants import ParseMode
 from telegram.ext import filters as filters_module
 
+from RUKA.database.sql.disable_sql import checkdisable, sql_disable, sql_enable
 
 DISABLE_CMDS = []
+DISABLED_CHATS = {}
+
 
 a = 2
 if a == 2:
@@ -29,27 +32,12 @@ if a == 2:
 
     def check_update(self, update):
         chat_id = update.effective_chat.id
-        #if chat_id in disabled_commands and disabled_commands[chat_id] == self.command[0]:
-        #    return False
+        disabled_cmd = checkdisable(chat_id)
+        print(self.command[0])
+        if self.command[0] in disabled_cmd:
+            return False
         return super().check_update(update)
 
-    """
-    class disablecommandhandler(CommandHandler):
-        def __init__(self, command, callback, block = True, filters: filters_module.BaseFilter = None):
-
-            if type(command) == list:
-                for comnd in command:
-                    comnd = comnd.lower()
-                    DISABLE_CMDS.append(comnd)
-            elif type(command) == str:
-                command = command.lower()
-                DISABLE_CMDS.append(command)
-            else:
-                LOGGER.error(f"Command: {command} is not a valid bot command")
-
-            print("done")
-            print(DISABLE_CMDS)
-    """
 
 else:
     disablecommandhandler = CommandHandler
