b6e5c184ba64f62387cc97d4ed19c0e0c66d67a3
Ishikki Akabane <rohansjaishwal@gmail.com>
2023-06-05 20:13:23 +0530

    Update __main__.py

--- RUKA/__main__.py
+++ RUKA/__main__.py
@@ -66,6 +66,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         )
 
 # Create the menu function
+"""
 def create_menu():
     keyboard = []
     modules_dir = 'RUKA/modules'
@@ -94,6 +95,41 @@ def create_menu():
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     # Send the menu message
+    return reply_markup
+"""
+
+def create_menu():
+    keyboard = []
+    modules_dir = 'RUKA/modules'
+
+    # Iterate over the module files
+    for filename in os.listdir(modules_dir):
+        if filename.endswith('.py'):
+            # Construct the module name from the file name
+            module_name = filename[:-3]
+
+            # Load the module dynamically
+            module = importlib.import_module(f'RUKA.modules.{module_name}')
+
+            # Get the values of __mod_name__ and __help__ variables from the module
+            mod_name = getattr(module, '__mod_name__', module_name)
+            help_text = getattr(module, '__help__', 'No help available.')
+
+            # Create the button with the module name as text and module_name as data
+            button = {
+                'text': mod_name,
+                'callback_data': f'module:{module_name}'
+            }
+            keyboard.append([button])
+
+    # Reshape the keyboard into 3 x 6 layout
+    keyboard = [keyboard[i:i + 6] for i in range(0, len(keyboard), 6)]
+
+    # Create the InlineKeyboardMarkup with the keyboard layout
+    reply_markup = {
+        'inline_keyboard': keyboard
+    }
+
     return reply_markup
 
 
