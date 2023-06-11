347b5a93dc65392f880b7abb0af2fb75825740a3
Sugar <108065240+sugar-Ik@users.noreply.github.com>
2023-06-11 19:57:25 +0530

    Update ishikki.py

--- RUKA/modules/ishikki.py
+++ RUKA/modules/ishikki.py
@@ -17,4 +17,40 @@ async def ishikki(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await bot.send_message(OWNER_ID, text=f"hello, {user_id}")
 
 
+async def abcfunc(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message = update.effective_message
+
+    bot = context.bot
+    reply = message.reply_to_message
+    if not reply:
+        return await message.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴡɪᴛʜ `/paste`", parse_mode=ParseMode.MARKDOWN)
+
+    msg = await message.reply_text("ᴘᴀsᴛɪɴɢ...")
+    if reply.text:
+        content = str(reply.text)
+    elif reply.document:
+        document = reply.document
+        if document.file_size > 1048576:
+            return await msg.edit_text("ʏᴏᴜ ᴄᴀɴ ᴏɴʟʏ ᴘᴀsᴛᴇ ғɪʟᴇs sᴍᴀʟʟᴇʀ ᴛʜᴀɴ 1ᴍʙ.")
+        if not pattern.search(document.mime_type):
+            return await msg.edit_text("ᴏɴʟʏ ᴛᴇxᴛ ғɪʟᴇs ᴄᴀɴ ʙᴇ ᴘᴀsᴛᴇᴅ.")
+
+        file_id = document.file_id
+        # Get the file object using the file_id
+        file = await bot.get_file(file_id)
+        print(file)
+        #doc = await bot.download_file(document.file_id)
+        #doc = doc_file.download()
+        doc = file.file_path
+        print(doc)
+
+        async with aiofiles.open(doc, mode="r") as f:
+            content = await f.read()
+            
+    else:
+        return await message.reply_text("lol")
+    
+    await message.reply_text(content)
+
+dp.add_handler(CommandHandler("abc", abcfunc))
 dp.add_handler(DisableCommandHandler(["ishikki", "ishu"], ishikki))
