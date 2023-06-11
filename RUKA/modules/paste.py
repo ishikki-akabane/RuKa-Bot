c1b5c27597f24120bab42ca78c751a11ac0d04ec
Sugar <108065240+sugar-Ik@users.noreply.github.com>
2023-06-11 20:53:48 +0530

    Update paste.py

--- RUKA/modules/paste.py
+++ RUKA/modules/paste.py
@@ -31,7 +31,7 @@ async def isPreviewUp(preview: str) -> bool:
     return False
 
 
-#@capture_error
+
 async def paste_func(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message = update.effective_message
 
@@ -40,30 +40,26 @@ async def paste_func(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not reply:
         return await message.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴡɪᴛʜ `/paste`", parse_mode=ParseMode.MARKDOWN)
 
-    msg = await message.reply_text("ᴘᴀsᴛɪɴɢ...")
+    await message.reply_text("ᴘᴀsᴛɪɴɢ...")
     if reply.text:
         content = str(reply.text)
     elif reply.document:
         document = reply.document
         if document.file_size > 1048576:
-            return await msg.edit_text("ʏᴏᴜ ᴄᴀɴ ᴏɴʟʏ ᴘᴀsᴛᴇ ғɪʟᴇs sᴍᴀʟʟᴇʀ ᴛʜᴀɴ 1ᴍʙ.")
+            return await message.reply_text("ʏᴏᴜ ᴄᴀɴ ᴏɴʟʏ ᴘᴀsᴛᴇ ғɪʟᴇs sᴍᴀʟʟᴇʀ ᴛʜᴀɴ 1ᴍʙ.")
         if not pattern.search(document.mime_type):
-            return await msg.edit_text("ᴏɴʟʏ ᴛᴇxᴛ ғɪʟᴇs ᴄᴀɴ ʙᴇ ᴘᴀsᴛᴇᴅ.")
+            return await message.reply_text("ᴏɴʟʏ ᴛᴇxᴛ ғɪʟᴇs ᴄᴀɴ ʙᴇ ᴘᴀsᴛᴇᴅ.")
 
         file_id = document.file_id
         # Get the file object using the file_id
         file = await bot.get_file(file_id)
-
-        #doc = await bot.download_file(document.file_id)
-        #doc = doc_file.download()
         doc = await file.download_to_drive()
-
-
+        
         async with aiofiles.open(doc, mode="r") as f:
             content = await f.read()
         os.remove(doc)
     else:
-        return await msg.edit_text("not supported!")
+        return await message.reply_text("not supported!")
 
     link = await paste(content)
     preview = f"{link}/preview.png"
@@ -80,11 +76,11 @@ async def paste_func(update: Update, context: ContextTypes.DEFAULT_TYPE):
                     ]
                 )
             )
-            return await msg.delete()
+            return
         except Exception:
             pass
 
-    await msg.edit_text(
+    await message.reply_text(
         text="Your file is pasted on ezup.dev!\nBy: @Ishikki",
         reply_markup=InlineKeyboardMarkup(
             [
