4cc4e1a9e9d6ceb38f4844ebe54ecb36c7e59c72
Ishikki Akabane <rohansjaishwal@gmail.com>
2023-06-26 11:26:55 +0530

    Delete media.py

--- RUKA/media.py
+++
@@ -1,8 +0,0 @@
-# This file is licensed under the terms of the MIT license. Authority goes to original author Ishikki-Akabane
-
-"""
-Contains all pic and video link
-"""
-
-PM_START_PIC = ""
-GROUP_START_PIC = ""
