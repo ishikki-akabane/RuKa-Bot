00c6cd4fa7593fdd7c2db3c3af113205adf613a3
Ishikki Akabane <rohansjaishwal@gmail.com>
2023-06-28 17:38:49 +0530

    Update __init__.py

--- RUKA/database/__init__.py
+++ RUKA/database/__init__.py
@@ -1,1 +1,21 @@
 #Lazyyy
+import asyncio
+import sys
+
+from motor import motor_asyncio
+from RUKA import MONGO_DB_URI 
+from pymongo import MongoClient
+from pymongo.errors import ServerSelectionTimeoutError
+
+
+MONGO_PORT = "27017"
+MONGO_DB = "ishikki"
+
+
+motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
+db = motor[MONGO_DB]
+
+try:
+    asyncio.get_event_loop().run_until_complete(motor.server_info())
+except ServerSelectionTimeoutError:
+    sys.exit(log.critical("Can't connect to mongodb! Exiting..."))
