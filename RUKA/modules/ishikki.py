b1ddb534b68727dae1e4518777acec59da5631f9
Ishikki Akabane <rohansjaishwal@gmail.com>
2023-06-29 19:02:06 +0530

    Update ishikki.py

--- RUKA/modules/ishikki.py
+++ RUKA/modules/ishikki.py
@@ -20,49 +20,4 @@ async def ishikki(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await bot.send_message(OWNER_ID, text=f"hello, {user_id}")
 
 
-"""
-async def abcfunccc(update, context):
-    message = update.effective_message
-
-    bot = context.bot
-    reply = message.reply_to_message
-    if not reply or reply is None:
-
-
-        
-        return await message.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴡɪᴛʜ `/paste`", parse_mode=ParseMode.MARKDOWN)
-    else:
-        
-        await message.reply_text("ᴘᴀsᴛɪɴɢ...")
-    if reply.text:
-        content = str(reply.text)
-    elif reply.document:
-        document = reply.document
-        
-        if document.file_size > 1048576:
-            
-            return #await msg.edit_text("ʏᴏᴜ ᴄᴀɴ ᴏɴʟʏ ᴘᴀsᴛᴇ ғɪʟᴇs sᴍᴀʟʟᴇʀ ᴛʜᴀɴ 1ᴍʙ.")
-
-
-        file_id = document.file_id
-        # Get the file object using the file_id
-        
-        file = await bot.get_file(file_id)
-        
-        
-        doc = await file.download_to_drive()
-        #doc = doc_file.download()
-        #doc = file.file_path
-        #print(doc)
-
-        async with aiofiles.open(doc, mode="r") as f:
-            content = await f.read()
-            
-    else:
-        return await message.reply_text("lol")
-    
-    await message.reply_text(content)
-
-dp.add_handler(DisableCommandHandler("abc", abcfunccc, block=False))
-"""
 dp.add_handler(DisableCommandHandler(["ishikki", "ishu"], ishikki))
