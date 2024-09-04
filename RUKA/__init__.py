939575c17fa3c51bd93ef69c176b28584bbb5075
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-09-04 20:03:03 +0530

    Update __init__.py

--- RUKA/__init__.py
+++ RUKA/__init__.py
@@ -7,6 +7,12 @@ If you face any issues, feel free to visit @DevsLAB,
 or into my DM to abuse me or for help or just to say thanks.
 Thankyou if you read this notice fully :), have a wonderful cody day
 """
+
+
+"""
+This file imports all essential variables and functions, sets up logging, 
+and initializes key components like the uptime tracker.
+"""
 import logging
 import os
 import sys
@@ -14,9 +20,10 @@ import time
 import asyncio
 import aiohttp
 import json
+from .config import Config
 
 
-# Enable Logging========================================================================================X
+# Enable Logging ========================================================================================X
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     handlers=[logging.FileHandler("logs.txt"), logging.StreamHandler()],
@@ -24,18 +31,32 @@ logging.basicConfig(
 )
 LOGGER = logging.getLogger(__name__)
 
-# Python version must be above or equal to 3.7==========================================================X
+# Python version must be above or equal to 3.7 ==========================================================X
 if sys.version_info[0] < 3 or sys.version_info[1] < 6:
     LOGGER.error(
         "You MUST have a python version of at least 3.7! Multiple features depend on this. Bot quitting.",
     )
     quit(1)
 
-# Starts the clock to note since how long the bot is running============================================X
+# Starts the clock to note since how long the bot is running ============================================X
 StartTime = time.time()
 
 
-# ENVIRONMENT VARIABLE==================================================================================X
+# ENVIRONMENT VARIABLE ==================================================================================X
+TOKEN = Config.TOKEN
+API_ID = Config.API_ID
+API_HASH = Config.API_HASH
+BOT_USERNAME = Config.BOT_USERNAME
+DATABASE_URL = Config.DATABASE_URL
+LOG_CHANNEL = Config.LOG_CHANNEL
+SUDO_USERS = Config.SUDO_USERS
+OWNER_ID = Config.OWNER_ID
+SUPPORT_CHAT = Config.SUPPORT_CHAT
+TOKEN_EXAMPLE = Config.TOKEN_EXAMPLE
+
+LOGGER.info("[SERVER] - All variables successfully loaded")
+
+"""
 ENV = bool(os.environ.get("ENV", False))
 
 if ENV:
@@ -197,3 +218,4 @@ async def booting_msg(application: Application):
 
 # Build dispatcher object for python-telegram-bot
 dp = ApplicationBuilder().token(TOKEN).post_init(booting_msg).build()
+"""
