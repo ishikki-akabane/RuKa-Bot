2daff56e95a6bb8b19b6e0e1437fbe823a0fc3bf
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-09-10 15:54:22 +0530

    Update __init__.py

--- RUKA/database/__init__.py
+++ RUKA/database/__init__.py
@@ -4,8 +4,14 @@ from datetime import datetime
 from GramDB import GramDB, GramDBAsync
 from RUKA import DATABASE_URL, LOGGER
 
+from .user import UserOperations
+from .group import GroupOperations
 
-class DATABASE:
+
+class DATABASE(
+    UserOperations,
+    GroupOperations
+):
     """
     A class to manage the database operations using GramDB.
 
@@ -38,48 +44,6 @@ class DATABASE:
         for table_name, schema in self.table_schemas.items():
             if not await self.db.check_table(table_name):
                 await self.db.create(table_name, schema)
-
-    async def check_user(self, user_id: int):
-        data = await self.db.find_one(
-            "users",
-            {
-                "_id": user_id
-            }
-        )
-        if data:
-            return data
-        else:
-            return None
-                
-    async def add_user(
-        self,
-        user_id: int,
-        name: str,
-        bio: str,
-        coins: int,
-        is_scanned: bool
-    ):
-        data = await self.check_user(user_id)
-        if data:
-            return
-        else:
-            current_time = datetime.now()
-            str_date = current_time.strftime("%d %B, %Y")            
-            try:
-                await self.db.insert_one(
-                    "users",
-                    {
-                        "_id": user_id,
-                        "name": name,
-                        "bio": bio,
-                        "coins": coins,
-                        "joined_date": str_date,
-                        "is_scanned": is_scanned
-                    }
-                )
-            except Exception as e:
-                LOGGER.error(f"Error adding user to database: {e}")
-            return
 
 
 db = DATABASE(DATABASE_URL)
