9746e389bb8f5fb065e1ef59d539b779aba8f7db
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-09-10 15:47:05 +0530

    Create users.py

---
+++ RUKA/database/users.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+from RUKA import LOGGER
+
+class UserOperations:
+    """
+    A class that handles user-related operations.
+    """
+    async def check_user(self, user_id: int):
+        data = await self.db.find_one(
+            "users",
+            {
+                "_id": user_id
+            }
+        )
+        if data:
+            return data
+        else:
+            return None
+
+    async def add_user(
+        self,
+        user_id: int,
+        name: str,
+        bio: str,
+        coins: int,
+        is_scanned: bool
+    ):
+        data = await self.check_user(user_id)
+        if data:
+            return
+        else:
+            current_time = datetime.now()
+            str_date = current_time.strftime("%d %B, %Y")
+            try:
+                await self.db.insert_one(
+                    "users",
+                    {
+                        "_id": user_id,
+                        "name": name,
+                        "bio": bio,
+                        "coins": coins,
+                        "joined_date": str_date,
+                        "is_scanned": is_scanned
+                    }
+                )
+            except Exception as e:
+                LOGGER.error(f"Error adding user to database: {e}")
+            return
