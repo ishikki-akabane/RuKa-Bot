8d890ec4bcf8211633afa9a626d900549b6b4dfa
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-09-18 14:22:27 +0530

    Update start.py

--- RUKA/modules/start.py
+++ RUKA/modules/start.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from RUKA.helpers.error_logger import ErrorLogger
 
 
-#@ErrorLogger
 @Client.on_message(filters.command("start"))
+@ErrorLogger
 async def start_cmd(client, message):
     await message.reply_text("started...", haha="haha")
