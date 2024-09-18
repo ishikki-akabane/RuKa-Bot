411dddd0820136f5402ae2cb1a9ad1a3e3e8c919
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-09-18 13:47:57 +0530

    Create error_logger.py

---
+++ RUKA/helpers/error_logger.py
@@ -0,0 +1,26 @@
+# error logging decorator
+
+import traceback
+import requests
+from RUKA import TOKEN, ERROR_CHANNEL
+
+
+def ErrorLogger(func):
+    async def wrapper(client, message):
+        try:
+            await func(client, message)
+        except Exception as e:
+            # Capture the error details
+            error_message = (
+                f"Error in function '{func.__name__}':\n"
+                f"File: {func.__code__.co_filename}\n"
+                f"Line: {func.__code__.co_firstlineno}\n"
+                f"Error: {str(e)}\n"
+                f"Traceback:\n{traceback.format_exc()}"
+            )
+            print(error_message)
+            await message.reply_text(
+                "`oppss!! Something went wrong!\nPleasw try again later :(`"
+            )
+            
+    return wrapper
