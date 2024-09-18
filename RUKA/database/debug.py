41c567ec4e532fe5606159429dbee8de8f773f8a
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-09-18 20:24:34 +0530

    Create debug.py

---
+++ RUKA/database/debug.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from RUKA import LOGGER
+
+class DebugOperations:
+    """
+    A class that handles Debugging operations.
+    """
+    async def add_error(
+        self,
+        chat_id: int,
+        func_name: str,
+        file_path: str,
+        error_line: str,
+        error_e: str
+    ):
+        current_time = datetime.now()
+        str_date = current_time.strftime("%d %B, %Y %H:%M:%S")
+        try:
+            await self.db.insert_one(
+                "debug",
+                {
+                    "chat_id": chat_id,
+                    "func_name": func_name,
+                    "file_path": file_path,
+                    "error_line": error_line,
+                    "error_e": error_e
+                }
+            )
+        except Exception as e:
+            LOGGER.error(f"Error adding debug info to database: {e}")
+        return
