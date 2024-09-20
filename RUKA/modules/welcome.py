7dbe34e5527bf5a32650e32a92471cc40470a851
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-09-20 15:17:36 +0530

    Update welcome.py

--- RUKA/modules/welcome.py
+++ RUKA/modules/welcome.py
@@ -1,6 +1,8 @@
+
+
 from pyrogram import Client, filters
 
-@Client.on_message(filters.new_chat_members, group=4)
+#@Client.on_message(filters.new_chat_members, group=3)
 async def welcome_cmd(client, message):
     new_members = message.new_chat_members
     for member in new_members:
