cc0f3a1bdcbf4db4f28bce9aaf76082695028627
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-09-20 14:31:06 +0530

    Create welcome.py

---
+++ RUKA/modules/welcome.py
@@ -0,0 +1,9 @@
+from pyrogram import Client, filters
+
+@Client.on_message(filters.new_chat_members, group=4)
+async def welcome_cmd(client, message):
+    new_members = message.new_chat_members
+    for member in new_members:
+        bot_id = 123456789
+        if bot_id == member.id:
+            pass
