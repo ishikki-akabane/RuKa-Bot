cb757026ff47b03deee655eacf4cef6060dc13ec
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-09-20 16:26:02 +0530

    Update welcome.py

--- RUKA/modules/welcome.py
+++ RUKA/modules/welcome.py
@@ -2,10 +2,14 @@
 
 from pyrogram import Client, filters
 
-#@Client.on_message(filters.new_chat_members, group=3)
+from RUKA import BOT_ID
+from RUKA.database import db, CACHE_GROUPS
+
+
+@Client.on_message(filters.new_chat_members, group=3)
 async def welcome_cmd(client, message):
     new_members = message.new_chat_members
     for member in new_members:
-        bot_id = 123456789
-        if bot_id == member.id:
-            pass
+        if BOT_ID == member.id:
+            return
+        
