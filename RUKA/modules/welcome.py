2d06615a5101305604ceedfd3648dd39eb5fe716
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-09-22 14:06:07 +0530

    Update welcome.py

--- RUKA/modules/welcome.py
+++ RUKA/modules/welcome.py
@@ -1,6 +1,7 @@
 
 
 from pyrogram import Client, filters
+from pyrogram.enums import ChatMemberStatus, ChatType
 
 from RUKA import BOT_ID
 from RUKA.database import db, CACHE_GROUPS
@@ -15,13 +16,41 @@ db_structure = {
 
 # Deprecated
 # @Client.on_message(filters.new_chat_members, group=3)
+
+
 @Client.on_chat_member_updated(filters.group, group=3)
 async def welcome_cmd(client, member):
+    if (
+        not member.new_chat_member 
+        or member.new_chat_member.status in {ChatMemberStatus.BANNED, ChatMemberStatus.LEFT, ChatMemberStatus.RESTRICTED} 
+        or member.old_chat_member
+    ):
+        return
+
     chat_id = member.chat.id
     user = member.new_chat_member.user if member.new_chat_member else member.from_user
+
     if BOT_ID == user.id:
         await client.send_message(chat_id, "ruka hop in")
-        return
     else:
         await client.send_message(chat_id, f"{user.first_name} hopped in")
         
+
+@Client.on_chat_member_updated(filters.group, group=4)
+async def goodbye_cmd(client, member):
+    if (
+        not member.new_chat_member 
+        and member.old_chat_member 
+        and member.old_chat_member.status not in {CMS.BANNED, CMS.RESTRICTED}
+    ):
+        pass
+    else:
+        return
+        
+    chat_id = member.chat.id
+    user = member.old_chat_member.user if member.old_chat_member else member.from_user
+    
+    if BOT_ID == user.id:
+        await client.send_message(-1001703076744, f"{chat_id} kicked ruka and said see ya in hell")
+    else:
+        await client.send_message(chat_id, f"{user.first_name} byeee")
