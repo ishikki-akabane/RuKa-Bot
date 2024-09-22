ef8ee129573552230767ed43c6327704fa035350
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-09-22 15:08:25 +0530

    Create welcome.py

---
+++ RUKA/helpers/welcome.py
@@ -0,0 +1,13 @@
+
+
+
+async def fetch_welcome(welcome_data):
+    welcome_text = welcome_data["text"]
+    welcome_button = welcome_data["button"]
+    welcome_ = welcome_data[""]
+    
+    welcome_type = welcome_data["type"]
+    if welcome_type == "template":
+        data = aahhh nvm, i will do tomorrow 
+
+    
