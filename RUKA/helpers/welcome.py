e13aa8c44759c71e859a4f8d0c8bde331aa61d76
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-09-23 18:36:06 +0530

    Update welcome.py

--- RUKA/helpers/welcome.py
+++ RUKA/helpers/welcome.py
@@ -15,17 +15,22 @@ async def fetch_welcome(welcome_data):
 
 async def initialise_welcome(template_id, client, user, chat):
     data = MeowClient.check_welcome_template(template_id)
+    result = []
     if data["user_pfp"]
         try:
             user_pfp = user.photo.big_file_id
         except:
             return False, "couldn't fetch big_file_id for user pfp"
-        photo = await client.download_media(user_pfp, file_name=f"{user.id}userpfp.jpg")
+        userphoto = await client.download_media(user_pfp, file_name=f"{user.id}userpfp.jpg")
+        result.append(userphoto)
 
     if data["chat_pfp"]
         try:
             chat_pfp = chat.photo.big_file_id
         except:
-            return False, "couldn't fetch big_file_id for user pfp"
-        photo = await client.download_media(user_pfp, file_name=f"{user.id}userpfp.jpg")
+            return False, "couldn't fetch big_file_id for chat pfp"
+        chatphoto = await client.download_media(chat_pfp, file_name=f"{user.id}chatpfp.jpg")
+        result.append(chatphoto)
+
+    return True, result
     
