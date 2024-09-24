81f84e007923d3628ba68660e591a87fd586f2a4
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-09-24 16:15:04 +0530

    Update welcome.py

--- RUKA/helpers/welcome.py
+++ RUKA/helpers/welcome.py
@@ -10,14 +10,14 @@ async def initialise_welcome(template_id, client, user, chat):
             user_pfp = user.photo.big_file_id
         except:
             print(f"couldn't fetch big_file_id for user {user.id} pfp")
-        userphoto = await client.download_media(user_pfp, file_name=f"{user.id}userpfp.jpg")
+        userphoto = await client.download_media(user_pfp, file_name=f"../downloads/{user.id}userpfp.jpg")
 
     if data["data"]["chat_pfp"]:
         try:
             chat_pfp = chat.photo.big_file_id
         except:
             print(f"couldn't fetch big_file_id for chat {chat.id} pfp")
-        chatphoto = await client.download_media(chat_pfp, file_name=f"{user.id}chatpfp.jpg")
+        chatphoto = await client.download_media(chat_pfp, file_name=f"../downloads/{user.id}chatpfp.jpg")
 
     return data
     
