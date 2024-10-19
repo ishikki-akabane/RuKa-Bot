870341d0110b9fe77a38ef1e37832caa100d87d5
XZeipher <144370232+XZeipher@users.noreply.github.com>
2024-10-19 09:18:56 +0530

    Update admin_func.py

--- RUKA/helpers/admin_func.py
+++ RUKA/helpers/admin_func.py
@@ -1,13 +1,10 @@
-
-
 from pyrogram.enums import ChatMemberStatus
 from pyrogram.types import ChatPrivileges
 
 
 async def check_permissions(client, chat_id, user_id, mode):
     member = await client.get_chat_member(chat_id=chat_id, user_id=user_id)
-
-    if mode == "blahblah":
+    if mode == "admin":
         if member.status in [ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.OWNER]:
             if member.privileges.can_restrict_members:
                 return True
@@ -15,5 +12,12 @@ async def check_permissions(client, chat_id, user_id, mode):
                 return False
         else:
             return False
+    else:
+        return False
+
+async def check_admin(client, chat_id, user_id):
+    member = await client.get_chat_member(chat_id=chat_id,user_id=user_id)
+    if member.status in [ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.OWNER]:
+        return True
     else:
         return False
