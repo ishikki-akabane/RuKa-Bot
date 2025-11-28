6b9a63e881b4c19230ea1cea96c3b50e3088dddf
Ishikki Akabane <rohansjaishwal@gmail.com>
2025-11-28 18:59:20 +0100

    Update __init__.py

---
+++ RUKA/db/__init__.py
@@ -0,0 +1,17 @@
+# Ruka-Bot - Telegram Group Management Bot
+# Copyright (C) 2023-2025 Ishikki Akabane <https://github.com/ishikki-akabane>
+#
+# This file is part of Ruka-Bot.
+#
+# Ruka-Bot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ruka-Bot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Ruka-Bot.  If not, see <https://www.gnu.org/licenses/>.
